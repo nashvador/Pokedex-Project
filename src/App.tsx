f0d8eb414f5d4ef33299f35ec4dbd80ad21b4c2f
DavidCChi <davidchi8666@gmail.com>
2021-12-19 14:24:10 -0500

    Add prettier and reformat files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./Components/Navbar/Navbar";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Pokedex from "./Pages/Pokedex";
-import Teambuilder from "./Pages/Teambuilder";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/Navbar/Navbar';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Pokedex from './Pages/Pokedex';
+import Teambuilder from './Pages/Teambuilder';
 
 function App() {
   return (
