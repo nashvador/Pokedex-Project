f4ee0d46f46772d08026ee6094de971ac951576a
Nash Vador <nashvador@gmail.com>
2021-12-20 02:42:06 -0500

    Adding Gifs to Homescreen

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,25 @@
+import mew from './mew.gif';
+import mewtwo from './mewtwo.gif';
+import pikachu from './pikachu.gif';
+
 const Home = () => {
+  const DivStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  };
+  // Making elements larger
+  const ImageStyle = {
+    // width: '200px',
+    // height: '100px',
+  };
+
   return (
-    <div>
-      <h1>Home Screen</h1>
+    <div style={DivStyle}>
+      <img src={mew} alt="mew" style={ImageStyle} />
+      <img src={mewtwo} alt="mew" style={ImageStyle} />
+      <img src={pikachu} alt="mew" style={ImageStyle} />
     </div>
   );
 };
